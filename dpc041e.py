631aebb31c096c9a1ea50dd6c7f5e6eb19a3a076
CCMitracos <32472605+CCMitracos@users.noreply.github.com>
2017-10-26 16:47:44 -0700

    Create dpc041e.py

---
+++ dpc041e.py
@@ -0,0 +1,12 @@
+# DPC 041 [E]
+# Text Banner
+
+words = raw_input("words >> ")
+words_width = len(words)
+total_width = words_width + 6
+
+print "\n", "*" * total_width
+
+print "\n","* ", words, " *"
+
+print "\n", "*" * total_width
